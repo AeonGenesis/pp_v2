2cef36a5e11048f931574b761994572f54e2fff4
tabs <67957760+AeonGenesis@users.noreply.github.com>
2022-11-28 04:40:54 +0100

    Update V2_a4_quantify.java

--- V2_a4_quantify.java
+++ V2_a4_quantify.java
@@ -1,35 +1,36 @@
 public class V2_a4_quantify {
 
     public static void main(String[] args) {
-        abtast(0.1, 2, 4 );
+
+        abtast(0.1, 2, 4 ); //werte in abtast methode schicken
     }
-    private static double funktion(double x) 
+    private static double funktion(double x) //f(x) methode
     {
-        double y =  20 * (x * x) - 100 * x + 129.5;
+        double y =  20 * (x * x) - 100 * x + 129.5; 
         return y; 
     }
 
-    private static void abtast(double rate, double start, double end)
+    private static void abtast(double rate, double start, double end) //abtast methode
     {
-        double n = 0;
-        double result = 0; 
-        int quantized = 0; 
-        for(int i = 0; i < (int) (10*(end - start)+1); i++ )
-        {
+        double n;
+        double result; 
+        int quantized; 
 
-            n = (double) i; 
-            result = funktion(start + (n * rate));
-            quantized = (int) result;
+        for(int i = 0; i < (int) (10*(end - start)+1); i++ ){ //abtast-schritte von start bis ende 
+
+            n = (double) i; //cast i zu double; i wert in n speichern
+            result = funktion(start + (n * rate)); //start und rate in f(x) methode verrechnen
+            quantized = (int) result; //cast ergebnis zu int; in quantized speichern
             
-            System.out.println("Für den x-Wert: "+ (start+(n*rate)) +  " beträgt der Funktionswert f(X): " + result + ". Quantisiert: " + quantized + ".");
-            if ((start + (n * rate)) == 2.5)
-                System.out.println("An dieser Stelle befindet sich ein Minimum. ");
+            /* Ausgabe einzelner Abtastschritte */
+            System.out.println("x-Wert: "+ (start+(n*rate)) +  "    |   Funktionswert f(X): " + result + "  |   Quantisiert: " + quantized + "  |");
             
-        }
-        
-        
-    }
-
-
-    
+            /* Ausgabe Minimum */
+            if ((start + (n * rate)) == 2.5){
+                System.out.println("------------");
+               System.out.println("Minimum!");
+               System.out.println("------------");
+            }                
+        }      
+    }   
 }
