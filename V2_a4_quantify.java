a01014924c6761cfa83a3a167d1d4368dba2e4bf
tabs <67957760+AeonGenesis@users.noreply.github.com>
2022-11-28 21:25:18 +0100

    abtastkakk

--- V2_a4_quantify.java
+++ V2_a4_quantify.java
@@ -1,37 +1,19 @@
 public class V2_a4_quantify {
-
     public static void main(String[] args) {
-        /*Abtastrate 0.1 in Intervall [2 bis 4] */
-        abtast(0.1, 2, 4 ); //werte in abtast methode schicken
-
-    }
-    private static double funktion(double x) //f(x) methode
-    {
-        double y =  20 * (x * x) - 100 * x + 129.5; 
-        return y; 
+        abtasten(0.1, 2, 4);
     }
 
-    private static void abtast(double rate, double start, double end) //abtast methode
-    {
-        double n;
-        double result; 
-        int quantized; 
+    private static double f(double x) {
+        return 20 * (x * x) - 100 * x + 129.5;
+    }
 
-        for(int i = 0; i < (int) (10*(end - start)+1); i++ ){ //abtast-schritte von start bis ende 
-
-            n = (double) i; //cast i zu double; i wert in n speichern
-            result = funktion(start + (n * rate)); //start und rate in f(x) methode verrechnen
-            quantized = (int) result; //cast ergebnis zu int; in quantized speichern
-            
-            /* Ausgabe einzelner Abtastschritte */
-            System.out.println("x-Wert: "+ (start+(n*rate)) +  "    |   Funktionswert f(X): " + result + "  |   Quantisiert: " + quantized + "  |");
-            
-            /* Ausgabe Minimum */
-            if ((start + (n * rate)) == 2.5){
-                System.out.println("------------");
-               System.out.println("Minimum!");
-               System.out.println("------------");
-            }                
-        }      
-    }   
+    private static void abtasten(double rate, double start, double end) {
+        for (double i = start; i <= end; i += rate) {
+            i = Math.round(i * 100.0) / 100.0;
+            double res = f(i);
+            if (i == 2.5)
+                System.out.println("Minimum!");
+            System.out.println("x-Wert: " + i + " | f(x): " + res + " | Quantisiert: " + (int) res);
+        }
+    }
 }
